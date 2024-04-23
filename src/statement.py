af2dcedee3bc619ad6e48dabe398692bbd001f1b
gurvirdhillon17 <130307317+gurvirdhillon17@users.noreply.github.com>
2024-04-23 11:46:26 -0700

    Update statement.py
    if-else statemtns and while loop classes

--- src/statement.py
+++ src/statement.py
@@ -12,3 +12,16 @@ class Statement:
 
     def __repr__(self):
         return self.__str__()
+
+class IfStatement(Statement):
+    def __init__(self, condition, then_branch, else_branch=None):
+        super().__init__("IF_STATEMENT")
+        self.condition = condition
+        self.then_branch = then_branch
+        self.else_branch = else_branch
+
+class WhileStatement(Statement):
+    def __init__(self, condition, body):
+        super().__init__("WHILE_STATEMENT")
+        self.condition = condition
+        self.body = body
